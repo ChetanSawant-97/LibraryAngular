25c6755402bcffca58313d0034ca097861ec99b2
Chetan Sawant <sawantchetan8149@gmail.com>
2024-07-06 11:37:16 +0530

    First Commit

---
+++ src/lib/card/card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent implements OnInit {
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+}
---
+++ src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of common-utils
+ */
+export * from './lib/common-utils.service';
+export * from './lib/common-utils.component';
+export * from './lib/card/card.component'
